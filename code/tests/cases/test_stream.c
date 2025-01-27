724586a4245d71ff4f03fc52cbc9ea594191e0a9
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-01-26 18:46:44 -0600

    remove test cases

--- code/tests/cases/test_stream.c
+++ code/tests/cases/test_stream.c
@@ -106,49 +106,6 @@ FOSSIL_TEST_CASE(c_test_stream_seek_and_tell) {
     fossil_fstream_close(&c_stream);
 }
 
-FOSSIL_TEST_CASE(c_test_stream_read_buffered) {
-    const char *filename = "testfile_buffered.txt";
-    const char *content = "Buffered read test content.";
-    char buffer[1024];
-
-    // Write data to the file
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename, "w"));
-    fossil_fstream_write(&c_stream, content, strlen(content), 1);
-    fossil_fstream_close(&c_stream);
-
-    // Read data from the file with buffering
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_read_buffered(filename, buffer, strlen(content)));
-    ASSUME_ITS_EQUAL_CSTR(content, buffer);
-}
-
-FOSSIL_TEST_CASE(c_test_stream_write_buffered) {
-    const char *filename = "testfile_buffered_write.txt";
-    const char *content = "Buffered write test content.";
-
-    // Write data to the file with buffering
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_write_buffered(filename, content, strlen(content)));
-
-    // Read data from the file to verify
-    char buffer[1024];
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename, "r"));
-    fossil_fstream_read(&c_stream, buffer, sizeof(buffer), 1);
-    fossil_fstream_close(&c_stream);
-
-    ASSUME_ITS_EQUAL_CSTR(content, buffer);
-}
-
-FOSSIL_TEST_CASE(c_test_stream_lock_and_unlock) {
-    const char *filename = "testfile_lock.txt";
-
-    // Create and lock the file
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename, "w"));
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_lock(filename));
-    fossil_fstream_close(&c_stream);
-
-    // Unlock the file
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_unlock(filename));
-}
-
 FOSSIL_TEST_CASE(c_test_stream_get_type) {
     const char *filename = "testfile_type.txt";
 
@@ -160,71 +117,6 @@ FOSSIL_TEST_CASE(c_test_stream_get_type) {
     ASSUME_ITS_EQUAL_I32(2, fossil_fstream_get_type(filename));  // Regular file
 }
 
-FOSSIL_TEST_CASE(c_test_stream_check_permission) {
-    const char *filename = "testfile_permission.txt";
-
-    // Create the file
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename, "w"));
-    fossil_fstream_close(&c_stream);
-
-    // Check read and write permissions
-    ASSUME_ITS_EQUAL_I32(1, fossil_fstream_check_permission(filename, "r"));
-    ASSUME_ITS_EQUAL_I32(1, fossil_fstream_check_permission(filename, "w"));
-}
-
-FOSSIL_TEST_CASE(c_test_stream_set_and_get_permissions) {
-    const char *filename = "testfile_set_permissions.txt";
-
-    // Create the file
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename, "w"));
-    fossil_fstream_close(&c_stream);
-
-    // Set and get permissions
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_set_permissions(filename, 0644));
-    ASSUME_ITS_EQUAL_I32(0644, fossil_fstream_get_permissions(filename));
-}
-
-FOSSIL_TEST_CASE(c_test_stream_restrict_permissions) {
-    const char *filename = "testfile_restrict.txt";
-
-    // Create the file
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename, "w"));
-    fossil_fstream_close(&c_stream);
-
-    // Restrict permissions
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_restrict(filename));
-    ASSUME_ITS_EQUAL_I32(0600, fossil_fstream_get_permissions(filename));
-}
-
-FOSSIL_TEST_CASE(c_test_stream_check_ownership) {
-    const char *filename = "testfile_ownership.txt";
-
-    // Create the file
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename, "w"));
-    fossil_fstream_close(&c_stream);
-
-    // Check ownership
-    ASSUME_ITS_EQUAL_I32(1, fossil_fstream_check_ownership(filename));
-}
-
-FOSSIL_TEST_CASE(c_test_stream_compare_permissions) {
-    const char *filename1 = "testfile_compare1.txt";
-    const char *filename2 = "testfile_compare2.txt";
-
-    // Create the files
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename1, "w"));
-    fossil_fstream_close(&c_stream);
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_open(&c_stream, filename2, "w"));
-    fossil_fstream_close(&c_stream);
-
-    // Set permissions
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_set_permissions(filename1, 0644));
-    ASSUME_ITS_EQUAL_I32(0, fossil_fstream_set_permissions(filename2, 0644));
-
-    // Compare permissions
-    ASSUME_ITS_EQUAL_I32(1, fossil_fstream_compare_permissions(filename1, filename2));
-}
-
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
@@ -234,15 +126,7 @@ FOSSIL_TEST_GROUP(c_file_tests) {
     FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_let_open_and_close_file);
     FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_multiple_files);
     FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_seek_and_tell);
-    FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_read_buffered);
-    FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_write_buffered);
-    FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_lock_and_unlock);
     FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_get_type);
-    FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_check_permission);
-    FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_set_and_get_permissions);
-    FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_restrict_permissions);
-    FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_check_ownership);
-    FOSSIL_TEST_ADD(c_stream_suite, c_test_stream_compare_permissions);
 
     FOSSIL_TEST_REGISTER(c_stream_suite);
 }
