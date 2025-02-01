9f332b7b271bab1eefb5ac164a40837e2898271b
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-02-01 09:36:35 -0600

    test

--- code/tests/cases/test_network.c
+++ code/tests/cases/test_network.c
@@ -155,9 +155,9 @@ FOSSIL_TEST_GROUP(c_network_tests) {
     FOSSIL_TEST_ADD(c_network_suite, c_test_network_create_socket);
     FOSSIL_TEST_ADD(c_network_suite, c_test_network_bind);
     FOSSIL_TEST_ADD(c_network_suite, c_test_network_listen);
-    FOSSIL_TEST_ADD(c_network_suite, c_test_network_accept);
-    // FOSSIL_TEST_ADD(c_network_suite, c_test_network_send_receive);
-    // FOSSIL_TEST_ADD(c_network_suite, c_test_network_close);
+    // FOSSIL_TEST_ADD(c_network_suite, c_test_network_accept);
+    FOSSIL_TEST_ADD(c_network_suite, c_test_network_send_receive);
+    FOSSIL_TEST_ADD(c_network_suite, c_test_network_close);
 
     FOSSIL_TEST_REGISTER(c_network_suite);
 }
