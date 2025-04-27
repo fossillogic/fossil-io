f9fa61b824ac22da13bd47fe1d2c0b0ba2279be1
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-04-27 09:52:06 -0500

    Update network.c

--- code/logic/network.c
+++ code/logic/network.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <sys/time.h>    // For struct timeval
 #include <unistd.h>      // For close on POSIX systems
 
