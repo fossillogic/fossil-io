ead1123d5cf1d9bf2a78714030fcb558ff845e96
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-05-06 12:25:50 -0500

    extend api

--- code/logic/fossil/io/parser.h
+++ code/logic/fossil/io/parser.h
@@ -28,7 +28,8 @@ typedef enum {
     FOSSIL_IO_PARSER_FLOAT,   // Floating-point argument
     FOSSIL_IO_PARSER_DATE,    // Date argument
     FOSSIL_IO_PARSER_ARRAY,   // Array of values
-    FOSSIL_IO_PARSER_FEATURE  // Feature flag
+    FOSSIL_IO_PARSER_FEATURE, // Feature flag
+    FOSSIL_IO_PARSER_INVALID  // Invalid argument type
 } fossil_io_parser_arg_type_t;
 
 // Structure to represent each argument in the command
@@ -56,6 +57,21 @@ typedef struct fossil_io_parser_palette_s {
     char *description;                        // Palette description
     fossil_io_parser_command_t *commands; // List of commands
 } fossil_io_parser_palette_t;
+
+// ==================================================================
+// Global Variables
+// ==================================================================
+
+extern int FOSSIL_CLI_TOGGLE_DRY_RUN;
+extern int FOSSIL_CLI_TOGGLE_VERBOSE;
+extern int FOSSIL_CLI_TOGGLE_SANITY;
+
+extern char *FOSSIL_CLI_TOOL_NAME;
+extern char *FOSSIL_CLI_TOOL_VERSION;
+extern char *FOSSIL_CLI_TOOL_AUTHOR;
+extern char *FOSSIL_CLI_TOOL_LICENSE;
+extern char *FOSSIL_CLI_TOOL_DESCRIPTION;
+extern char *FOSSIL_CLI_TOOL_WEBSITE;
 
 // ==================================================================
 // Functions
