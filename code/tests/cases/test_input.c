cf00ecff35da65f3e5fd7cf515ee65aee550be4b
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-09-06 11:23:36 -0700

    Update test_input.c

--- code/tests/cases/test_input.c
+++ code/tests/cases/test_input.c
@@ -236,44 +236,6 @@ FOSSIL_TEST(c_test_io_register_keybinding_duplicate) {
     fossil_io_clear_keybindings();
 }
 
-FOSSIL_TEST(c_test_io_unregister_keybinding_success) {
-    fossil_io_clear_keybindings();
-    fossil_io_register_keybinding(42, "jump");
-    int result = fossil_io_unregister_keybinding(42);
-    ASSUME_ITS_EQUAL_I32(0, result);
-    const char *action = fossil_io_get_keybinding_action(42);
-    ASSUME_ITS_CNULL(action);
-    fossil_io_clear_keybindings();
-}
-
-FOSSIL_TEST(c_test_io_unregister_keybinding_not_found) {
-    fossil_io_clear_keybindings();
-    int result = fossil_io_unregister_keybinding(99);
-    ASSUME_ITS_EQUAL_I32(1, result); // Key not found
-    fossil_io_clear_keybindings();
-}
-
-// Callback for keybinding test
-static int cb_called = 0;
-static void cb(void) { cb_called = 1; }
-
-FOSSIL_TEST(c_test_io_register_keybinding_with_callback_success) {
-    fossil_io_clear_keybindings();
-    cb_called = 0;
-    int result = fossil_io_register_keybinding_with_callback(55, "shoot", cb);
-    ASSUME_ITS_EQUAL_I32(0, result);
-    fossil_io_process_keybinding(55);
-    ASSUME_ITS_EQUAL_I32(1, cb_called);
-    fossil_io_clear_keybindings();
-}
-
-FOSSIL_TEST(c_test_io_get_keybinding_action_not_found) {
-    fossil_io_clear_keybindings();
-    const char *action = fossil_io_get_keybinding_action(123);
-    ASSUME_ITS_CNULL(action);
-    fossil_io_clear_keybindings();
-}
-
 FOSSIL_TEST(c_test_io_process_keybinding_no_binding) {
     fossil_io_clear_keybindings();
     int result = fossil_io_process_keybinding(77);
@@ -330,10 +292,6 @@ FOSSIL_TEST_GROUP(c_input_tests) {
 
     FOSSIL_TEST_ADD(c_input_suite, c_test_io_register_keybinding_success);
     FOSSIL_TEST_ADD(c_input_suite, c_test_io_register_keybinding_duplicate);
-    FOSSIL_TEST_ADD(c_input_suite, c_test_io_unregister_keybinding_success);
-    FOSSIL_TEST_ADD(c_input_suite, c_test_io_unregister_keybinding_not_found);
-    FOSSIL_TEST_ADD(c_input_suite, c_test_io_register_keybinding_with_callback_success);
-    FOSSIL_TEST_ADD(c_input_suite, c_test_io_get_keybinding_action_not_found);
     FOSSIL_TEST_ADD(c_input_suite, c_test_io_process_keybinding_no_binding);
     FOSSIL_TEST_ADD(c_input_suite, c_test_io_list_keybindings_populates_array);
     FOSSIL_TEST_ADD(c_input_suite, c_test_io_clear_keybindings_removes_all);
