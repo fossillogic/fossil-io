ccb5f5d98a1319ed305b84ec5b69cab4c367f53c
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-09-08 10:25:45 -0700

    Update test_soap.c

--- code/tests/cases/test_soap.c
+++ code/tests/cases/test_soap.c
@@ -172,7 +172,7 @@ FOSSIL_TEST(c_test_io_soap_detect_quality_true) {
 }
 
 FOSSIL_TEST(c_test_io_soap_detect_quality_false) {
-    const char *input = "Wow, this is amazing and unbelievable!";
+    const char *input = "Wow, this is amazing!";
     int result = fossil_io_soap_detect_quality(input);
     ASSUME_ITS_FALSE(result);
 }
