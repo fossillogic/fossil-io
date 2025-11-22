dea5d700d6787b196472ed4617945cad64a17424
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-11-21 18:17:49 -0600

    this time

--- code/logic/dir.c
+++ code/logic/dir.c
@@ -29,6 +29,7 @@
 #include <time.h>
 #include <errno.h>
 #include <limits.h>
+#include <stdlib.h> // for realpath, getenv, mkdtemp
 
 #ifdef _WIN32
   #define WIN32_LEAN_AND_MEAN
@@ -914,7 +915,6 @@ int32_t fossil_io_dir_get_created(const char *path, uint64_t *timestamp){
 #ifndef _WIN32
     // POSIX does not generally expose creation time; return ctime as approximation
     stat_t st;
-    if (stat_native(path, &st) != 0) return -1;
 #ifdef __APPLE__
     *timestamp = (uint64_t)st.st_birthtimespec.tv_sec;
 #else
