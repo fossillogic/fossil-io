c88a68dd4f8f5b4d92a8dfd3d232f13713f384b3
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-12-29 13:24:18 -0600

    Update test_soap.cpp

--- code/tests/cases/test_soap.cpp
+++ code/tests/cases/test_soap.cpp
@@ -54,6 +54,8 @@ FOSSIL_TEARDOWN(cpp_soap_suite) {
 // as samples for library usage.
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
+using namespace fossil::io;
+
 FOSSIL_TEST(cpp_soap_options_default_zeroed) {
     Soap::Options opt;
 
